a651ab4b9cc51f798e8169b196c1cdbbea26cd27
SolyGh <125503517+SolyGh@users.noreply.github.com>
2024-04-19 18:05:05 +0200

    done separated 3 files csvs ggl mcsft aapl

--- data/raw/financial_news_data.py
+++ data/raw/financial_news_data.py
@@ -7,28 +7,38 @@ newsapi = NewsApiClient(api_key=api_key)
 
 try:
     # Define keywords and sources for news articles
-    keywords = ['apple stock price news', 'Microsoft stock price news', 'Google stock price news', 'finance Egypt', 'economic Egypt', 'business Egypt']
-    sources = 'bbc-news,the-verge,aj-news,financial-times,news24,news-com,the-telegraph,alahram,al-jazeera-english,esquire,google-news,associated-press,al-jazeera-english,esquire,google-news,associated-press,al-jazeera-english,esquire,google-news,associated-press'
-
-    # Fetch news articles based on keywords and sources
-    articles = []
-    for keyword in keywords:
-        news = newsapi.get_everything(q=keyword, sources=sources, language='en', page_size=100)
-        articles.extend(news['articles'])
-
-    # Extract relevant data from news articles
-    data = {
-        'date': [article['publishedAt'] for article in articles],
-        'content': [article['content'] for article in articles],
-        'headlines': [article['title'] for article in articles]
+    keywords = {
+        'apple': ['apple stock price news'],
+        'microsoft': ['Microsoft stock price news'],
+        'google': ['Google stock price news']
     }
+    
+    sources = 'bbc-news,the-verge,aj-news,financial-times,news24,news-com,the-telegraph,alahram,al-jazeera-english,esquire,google-news,associated-press'
 
-    # Create a DataFrame from the extracted data
-    df = pd.DataFrame(data)
+    # Fetch news articles based on keywords and sources for each company
+    for company, keyword_list in keywords.items():
+        articles = []
+        for keyword in keyword_list:
+            news = newsapi.get_everything(q=keyword, sources=sources, language='en', page_size=100)
+            articles.extend(news['articles'])
 
-    # Save the DataFrame to a CSV file in the raw data folder
-    df.to_csv('../data/raw/financial_news_data.csv', index=False)
-    print("Financial news data saved successfully.")
+        # Extract relevant data from news articles
+        data = {
+            'date': [article['publishedAt'] for article in articles],
+            'content': [article['content'] for article in articles],
+            'headlines': [article['title'] for article in articles]
+        }
+
+        # Create a DataFrame from the extracted data
+        df = pd.DataFrame(data)
+
+        # Define the filename based on the company
+        filename = f'{company}-news_data.csv'
+
+        # Save the DataFrame to a CSV file
+        df.to_csv(filename, index=False)
+
+        print(f"{company.capitalize()} news data saved successfully.")
 
 except Exception as e:
     print("Error occurred:", str(e))
