da04d8787309dfa9cf07c9deea998f4aa77b9f18
Abdelrahman Ragab <abdelrahmanragab787@gmail.com>
2024-04-19 17:14:33 +0000

    fix directory

--- data/raw/historical_Data/historical_exchange_rates.py
+++ data/raw/historical_Data/historical_exchange_rates.py
@@ -1,9 +1,8 @@
-# Import necessary libraries
 import yfinance as yf
+from datetime import datetime
 
-# Fetch historical USD to EGP exchange rate data
-usd_to_egp_data = yf.download('EGP=X', start='2020-01-01', end='2024-04-14')  # Adjust start and end dates as needed
+# Fetch historical USD to EGP exchange rate data up to the current date and time
+usd_to_egp_data = yf.download('EGP=X', start='2020-01-01', end=datetime.now())  
 
 # Save the data to a CSV file
-usd_to_egp_data.to_csv('../data/raw/USD_to_EGP_exchange_rate.csv')
-
+usd_to_egp_data.to_csv('USD_to_EGP_exchange_rate.csv')
