bf0d588c8329f8ddc2b8594e856e208954815d94
Abdelrahman Ragab <abdelrahmanragab787@gmail.com>
2024-04-20 13:56:17 +0000

    add a 'Change (%)' column

--- data/raw/historical_Data/raw_data.py
+++ data/raw/historical_Data/raw_data.py
@@ -1,8 +1,8 @@
-# import pandas as pd
+ import pandas as pd
 
-# def load_stock_data(ticker):
-#     filename = f"data/raw/{ticker}_historical_stock_prices.csv"
-#     return pd.read_csv(filename)
+ def load_stock_data(ticker):
+     filename = f"{ticker}_historical_stock_prices.csv"
+     return pd.read_csv(filename)
 
-# def load_exchange_rate_data():
-#     return pd.read_csv("data/raw/USD_to_EGP_exchange_rate.csv")
+ def load_exchange_rate_data():
+     return pd.read_csv("USD_to_EGP_exchange_rate.csv")
